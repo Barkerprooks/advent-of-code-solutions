981ab4ab57ecb145ce83b8dd036936a23f746736
parker <jonparkerbrooks@gmail.com>
2020-12-10 22:26:18 -0600

    day 7 complete

--- day/7/main.jl
+++ day/7/main.jl
@@ -1,4 +1,4 @@
-function find_gold(key, bags, gold=[])
+function find_gold(key, bags, gold)
 	for b in bags[key]
 		b = b[3:length(b)]
 		if occursin("shiny gold", b)
@@ -18,9 +18,36 @@ function find_gold(key, bags, gold=[])
 	false
 end
 
+function search_for_gold(bags)
+	gold = []
+	for bag in bags
+		find_gold(bag.first, bags, gold)
+	end
+	gold
+end
+
+function get_ncnt(key, bags)
+	
+	total = 0
+
+	for bag in bags[key]
+		if occursin("other", bag)
+			return 0
+		else
+			n = parse(Int64, bag[1:2])
+			b = bag[3:length(bag)]
+			total += n
+			total += n * get_ncnt(b, bags)
+		end
+	end
+	
+	total
+
+end
+
 bags = Dict()
 
-open("input.txt", "r") do fd
+open("test.txt", "r") do fd
 	while ! eof(fd)
 		line = readline(fd)
 		pair = split(line, "contain")
@@ -30,9 +57,7 @@ open("input.txt", "r") do fd
 	end
 end
 
-gold = []
-for bag in bags
-	find_gold(bag.first, bags, gold)
-end
+gold = search_for_gold(bags)
 
 println("solution 1: ", length(gold))
+println("solution 2: ", get_ncnt("shiny gold", bags))
